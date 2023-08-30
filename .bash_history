e0e84441bcead228ed977f67a088a7b9f2b8df3d
Jarod <Jarod@MSI.edu>
2023-08-30 11:54:43 -0500

    Help

---
+++ .bash_history
@@ -0,0 +1,14 @@
+pwd
+cd ~
+cd
+cd
+pwd
+pwd
+git push -m Oops
+git push
+git push -m oops
+git push
+git commit
+git push
+pwd
+cd /c/Users/Jarod/OneDrive/Documents/Mec549/jarod_crider_unmanned_systems
--- jarod_crider_unmanned_systems/week_1/Hw1pr4.py
+++ jarod_crider_unmanned_systems/week_1/Hw1pr4.py
@@ -8,50 +8,34 @@ Created on Wed Aug 23 10:44:50 2023
 import matplotlib.pyplot as plt
 import numpy as np
 
-
+# Compute Index based on grid size and location
 def compute_index(min_x:int, max_x:int, min_y:int, max_y, gs:int, x_curr:int, y_curr:int) -> int:
     
     index = ((x_curr - min_x)/gs) + (((y_curr - min_y)/gs) * ((max_x-min_x+gs)/gs))
     
-    
-
-    return index
-
-def updated_index(min_x:int, min_y:int, x_curr:int, y_curr:int, gs:int, x_size:int) -> float:
-    index = ((x_curr - min_x)/gs) + (((y_curr - min_y)/gs) * x_size)
-
     return index
 
 
-
-
-
-
-if __name__ == '__main__':
     
-    
-    min_x = 0
-    max_x = 10
-    min_y = 0
-    max_y = 10
-    gs = 0.5
-    x_array = []
-    x_curr = 7
-    y_curr = 3.5
+min_x = 0
+max_x = 10
+min_y = 0
+max_y = 10
+gs = 0.5
+x_array = []
+x_curr = 7
+y_curr = 3.5
 
-    index = compute_index(min_x,max_x,min_y,max_y,gs,x_curr,y_curr)
-    print(index)
-        
-    improved_val = updated_index(min_x, min_y, x_curr, y_curr, gs, len(x_array))
-    print(improved_val)
+index = compute_index(min_x,max_x,min_y,max_y,gs,x_curr,y_curr)
+print(index)
+       
     
-    
-    
-    x_cord = list(np.arange(min_x,max_x+gs,gs))
-    y_cord = list(np.arange(min_y,max_y+gs,gs))
+# Value input for graphing    
+x_cord = list(np.arange(min_x,max_x+gs,gs))
+y_cord = list(np.arange(min_y,max_y+gs,gs))
 
-    for j, y in enumerate(y_cord):
-        for i, x in enumerate(x_cord):
-            index = compute_index(min_x,max_x,min_y,max_y,gs,x,y)
-            plt.text(x, y, str(int(index)), color="red", fontsize=64)
+for j, y in enumerate(y_cord):
+    for i, x in enumerate(x_cord):
+        index = compute_index(min_x,max_x,min_y,max_y,gs,x,y)
+        plt.text(x, y, str(int(index)), color="red", fontsize=64)
 
